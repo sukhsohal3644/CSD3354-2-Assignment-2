91b6e0d8665388ce0c3c4c8bad8bd2c7b1210ac5
visualstudio <visualstudio@230-PC44>
2019-03-07 14:49:05 -0500

    ccc

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -25,3 +25,60 @@ namespace DelegateAndEvents
     }
 
 }
+
+    // LAB 2
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+     {
+        public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate(50);
+            
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // TO DO: Call Method2 from the DelegateExercises Class
+    }
+}
+
+
+// LAB 3
+
+public class DelegateExercises
+{
+    public delegate void MyDelegate();
+    void Method1(int i)
+    {
+        Console.WriteLine("Method1");
+        Console.ReadLine();
+    }
+    public void Method2()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        myDelegate();
+    }
+}
+
+
+
+
