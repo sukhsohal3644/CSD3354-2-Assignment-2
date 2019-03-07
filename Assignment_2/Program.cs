8faab4039c1886c1f9bc81487475acd450e0e352
visualstudio <visualstudio@230-PC44>
2019-03-07 15:08:05 -0500

    jjj

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -34,7 +34,6 @@ namespace DelegateAndEvents
         }
 
         public void Method2()
-
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
             myDelegate();
@@ -42,18 +41,6 @@ namespace DelegateAndEvents
     }
 
 }
-
-// Student Name = Sukhdeep Sohal     Student ID = C0731586
-// CSD3354 Section 2
-// Assignment 2
-// March 7,2019
-
-
-
-// Student Name = Manjot Singh       Student ID = C0730244
-// CSD3354 Section 2
-// Assignment 2
-// March 7,2019
 
 
 
