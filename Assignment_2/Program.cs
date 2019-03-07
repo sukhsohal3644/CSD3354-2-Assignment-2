8711459950f6d90ff7d01b42b118f94bddb29fd5
visualstudio <visualstudio@230-PC44>
2019-03-07 16:55:18 -0500

    lab 14

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -16,15 +16,14 @@ using System.Threading.Tasks;
 // CSD3354 Section 2
 // Assignment 2
 // March 7,2019
-namespace DelegateAndEvents
+namespace DelegatesAndEvents
 {
     class program
     {
         public static void Main()
         {
-
-            DelegateExercises newDel = new DelegateExercises();
-            newDel.Method3();
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
             Console.ReadLine();
         }
     }
@@ -33,7 +32,7 @@ namespace DelegateAndEvents
     {
         int Method1(out int i)
         {
-            System.Console.WriteLine("Method1");
+            System.Console.WriteLine("Method1" + i);
             i = 10;
             return 0;
         }
@@ -41,9 +40,10 @@ namespace DelegateAndEvents
     public void Method3()
     {
         MyDelegate myDelegate = new MyDelegate(Method1);
-        MyDelegate myDelegate1 = new MyDelegate(Method1);
+        MyDelegate myDelegate1 = null;
         MyDelegate myDelegate2 = myDelegate + myDelegate1;
         int intValue;
         myDelegate2(out intValue);
     }
+
 }
